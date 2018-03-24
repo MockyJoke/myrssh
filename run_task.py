0350fc334d9e1fbd15e55827743495c2a28600d2
Johnson Pan <panjunsheng@msn.com>
2018-03-24 15:28:31 -0700

    Update run_task.py

--- run_task.py
+++ run_task.py
@@ -21,7 +21,8 @@ def main():
     elif task["mode"]=="script":
         proc = subprocess.Popen(["/bin/bash","-c ","\'" + task["content"] + "\'"] , stdout=subprocess.PIPE)
     std, err = proc.communicate()
-    print(std)    
-
+    print(std)
+    result = {"request" : task, "output" : std}
+    requests.post("https://hitmanservice.azurewebsites.net/api/queue/boss-pi-back",data = result)
 if __name__ == "__main__":
     main()
