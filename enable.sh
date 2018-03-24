18556e1722d68a4f0331c56ef7546022c3b59d89
Johnson Pan <panjunsheng@msn.com>
2018-03-24 15:05:53 -0700

    Update enable.sh

--- enable.sh
+++ enable.sh
@@ -4,7 +4,7 @@
 sudo crontab -l > mycron
 
 #echo new cron into cron file
-sudo echo "*/1 * * * * /usr/bin/python $(pwd)/run_task.py > $(pwd)/run_task.log" >> mycron
+sudo echo "*/1 * * * * /usr/bin/python $(pwd)/run_task.py >> $(pwd)/run_task.log" >> mycron
 
 #install new cron file
 sudo crontab mycron
